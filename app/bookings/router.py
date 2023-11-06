4ef5a3eb35169af0db453b53e1e816f8dd45f698
Борис Жуков <boris10264@gmail.com>
2023-11-06 14:52:16 +0300

    add result to get_bookings

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,4 +1,8 @@
 from fastapi import APIRouter
+from sqlalchemy import select
+
+from app.bookings.models import Bookings
+from app.database import async_session_maker
 
 router = APIRouter(
     prefix='/bookings',
@@ -7,10 +11,8 @@ router = APIRouter(
 
 
 @router.get('')
-def get_bookings():
-    pass
-
-
-@router.get('/{booking_id}')
-def get_booking(booking_id):
-    pass
+async def get_bookings():
+    async with async_session_maker() as session:
+        query = select(Bookings)
+        result = await session.execute(query)
+        return result.mappings().all()
