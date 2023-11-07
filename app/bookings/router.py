45ce118f44fbd61d6b7a28fcb0ede94f22c92ca2
Борис Жуков <boris10264@gmail.com>
2023-11-07 19:50:25 +0300

    add services to bookings

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from sqlalchemy import select
 
 from app.bookings.models import Bookings
+from app.bookings.service import BookingService
 from app.database import async_session_maker
 
 router = APIRouter(
@@ -12,7 +13,4 @@ router = APIRouter(
 
 @router.get('')
 async def get_bookings():
-    async with async_session_maker() as session:
-        query = select(Bookings)
-        result = await session.execute(query)
-        return result.mappings().all()
+    return await BookingService.find_all()
---
+++ app/service/base.py
@@ -0,0 +1,4 @@
+
+
+class BaseService:
+    model = None
