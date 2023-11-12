ad505ef0e6bc8047a7a188f4d9cf6d78a73514b0
Борис Жуков <boris10264@gmail.com>
2023-11-12 21:17:05 +0300

    add create access token

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,11 +1,26 @@
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
 
 pwd_contex = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+
 
 def get_password_hash(password: str) -> str:
-    return  pwd_contex.hash(password)
+    return pwd_contex.hash(password)
 
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_contex.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(
+        to_encode, SECRET_KEY, ALGORITHM
+    )
+    return encoded_jwt
