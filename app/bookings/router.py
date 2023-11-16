f6df2731eb9bd6d7fd64db248462fba91a117672
Борис Жуков <boris10264@gmail.com>
2023-11-16 21:04:34 +0300

    add SBookings validation

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -4,7 +4,7 @@ from app.bookings.schemas import SBookings
 from app.bookings.service import BookingService
 from app.users.dependencies import get_current_user
 from app.users.models import Users
-from app.users.service import UsersService
+
 
 router = APIRouter(
     prefix='/bookings',
@@ -13,6 +13,5 @@ router = APIRouter(
 
 
 @router.get('')
-async def get_bookings(user: Users = Depends(get_current_user)):  # -> list[SBookings]:
-    print(user, type(user), user.email)
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBookings]:
     return await BookingService.find_all(user_id=user.id)
