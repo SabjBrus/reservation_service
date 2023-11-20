25030a439dd2fce93b7d7115b37d6f24f490a893
Борис Жуков <boris10264@gmail.com>
2023-11-20 20:05:49 +0300

    add Hotels schema

---
+++ app/hotels/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class SHotels(BaseModel):
+    id: int
+    name: str
+    location: str
+    services: list[str]
+    rooms_quantity: int
+    image_id: int
+
+    class Config:
+        orm_mode = True
