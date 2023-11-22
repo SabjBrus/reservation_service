079fa64d7e2112528bebc0a323a75b4db831b446
Борис Жуков <boris10264@gmail.com>
2023-11-22 15:57:25 +0300

    add cors

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.pages.router import router as router_pages
@@ -20,6 +21,18 @@ app.include_router(router_bookings)
 app.include_router(router_pages)
 app.include_router(router_rooms)
 app.include_router(router_images)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # class HotelsSearchArgs:
