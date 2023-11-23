e7cb187ed0dec7b2ed80d10fa7d993b30e4a6e78
Борис Жуков <boris10264@gmail.com>
2023-11-23 19:48:49 +0300

    send_booking_confirmation_email

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,6 +1,13 @@
+import smtplib
+
+from pydantic import EmailStr
+
+from app.config import settings
 from app.tasks.celery import celery
 from PIL import Image
 from pathlib import Path
+
+from app.tasks.email_templates import create_booking_confirmation_template
 
 
 @celery.task
@@ -14,3 +21,15 @@ def process_pic(
     im_resized_1000_500.save(f'app/static/images/resized_1000_500_{im_path.name}')
     im_resized_200_100.save(f'app/static/images/resized_200_100_{im_path.name}')
 
+
+@celery.task
+def send_booking_confirmation_email(
+    booking: dict,
+    email_to: EmailStr,
+):
+    email_to_mock = settings.SMTP_USER  # отправка email себе
+    msg_content = create_booking_confirmation_template(booking, email_to_mock)
+
+    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_HOST) as server:
+        server.login(settings.SMTP_USER, settings.SMTP_PASS)
+        server.send_message(msg_content)
