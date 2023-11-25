db0256d3451478cba3c46d15d69c572aac659636
Борис Жуков <boris10264@gmail.com>
2023-11-25 12:02:51 +0300

    add relationship and admin to Users and Bookings

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from datetime import date
-from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
+from sqladmin import Admin
 
+from app.admin.views import UsersAdmin, BookingsAdmin
 from app.bookings.router import router as router_bookings
 from app.config import settings
+from app.database import engine
 from app.pages.router import router as router_pages
 from app.hotels.rooms.router import router as router_rooms
 from app.users.router import router as router_users
@@ -51,22 +52,8 @@ def startup():
     redis = aioredis.from_url(f'redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}')
     FastAPICache.init(RedisBackend(redis), prefix='fastapi-cache')
 
-# class HotelsSearchArgs:
-#     def __init__(
-#         self,
-#         location: str,
-#         date_from: date,
-#         date_to: date,
-#         has_spa: Optional[bool] = None,
-#         stars: Optional[int] = Query(None, ge=1, le=5),
-#     ):
-#         self.location = location
-#         self.date_from = date_from
-#         self.date_to = date_to
-#         self.has_spa = has_spa
-#         self.stars = stars
-#
-#
-# @app.get('/hotels')
-# def get_hotels(search_args: HotelsSearchArgs = Depends()):
-#     return search_args
+
+admin = Admin(app, engine)
+
+admin.add_view(UsersAdmin)
+admin.add_view(BookingsAdmin)
