28644541f194092f12b47c0ecbdf2cc1795cf760
Борис Жуков <boris10264@gmail.com>
2023-11-27 20:32:35 +0300

    add session and asyncckient fixtures

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -13,6 +13,12 @@ from app.users.models import Users
 from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
 
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from app.main import app as fastapi_app
+
+
 
 @pytest.fixture(scope='session', autouse=True)
 async def prepare_database():
@@ -23,7 +29,7 @@ async def prepare_database():
         await conn.run_sync(Base.metadata.create_all)
 
     def open_mock_json(model: str):
-        with open(f'app/tests/mock_{model}.json', 'r') as file:
+        with open(f'app/tests/mock_{model}.json', encoding='utf-8') as file:
             return json.load(file)
 
     hotels = open_mock_json('hotels')
@@ -56,3 +62,15 @@ def event_loop (request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope='function')
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url='http://test') as ac:
+        yield ac
+
+
+@pytest.fixture(scope='function')
+async def session():
+    async with async_session_maker() as session:
+        yield session
