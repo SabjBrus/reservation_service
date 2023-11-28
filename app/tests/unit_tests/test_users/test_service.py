e05242af2055013d3da960ecf753a9fa273f2241
Борис Жуков <boris10264@gmail.com>
2023-11-28 18:42:27 +0300

    fix mock data and add test_add_and_get_booking integration api

--- app/tests/unit_tests/test_users/test_service.py
+++ app/tests/unit_tests/test_users/test_service.py
@@ -6,7 +6,7 @@ from app.users.service import UsersService
 @pytest.mark.parametrize('user_id, email, exists', [
     (1, 'test1@test.com', True),
     (2, 'test2@test.com', True),
-    (3, 'wrong@email.com', False),
+    (88, 'wrong@email.com', False),
 ])
 async def test_find_user_by_id(user_id, email, exists):
     user = await UsersService.find_by_id(user_id)
