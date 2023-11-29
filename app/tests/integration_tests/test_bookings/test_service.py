14c5c41a981a228c3a5606d687d01846865ec1db
Борис Жуков <boris10264@gmail.com>
2023-11-29 18:14:23 +0300

    add test_add_get_and_delete_booking

--- app/tests/integration_tests/test_bookings/test_service.py
+++ app/tests/integration_tests/test_bookings/test_service.py
@@ -17,3 +17,22 @@ async def test_add_and_get_booking():
     new_booking = await BookingService.find_by_id(new_booking.id)
 
     assert new_booking is not None
+
+
+async def test_add_get_and_delete_booking():
+    new_booking = await BookingService.add(
+        user_id=2,
+        room_id=1,
+        date_from=datetime.strptime('2023-08-02', '%Y-%m-%d'),
+        date_to=datetime.strptime('2023-08-22', '%Y-%m-%d'),
+    )
+
+    new_booking = await BookingService.find_by_id(new_booking.id)
+
+    assert new_booking is not None
+
+    await BookingService.delete(id=new_booking.id)
+
+    new_booking = await BookingService.find_by_id(new_booking.id)
+
+    assert new_booking is None
