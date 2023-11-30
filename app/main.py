2414d7ea9963280f306ca6a470e56d33fd441147
Борис Жуков <boris10264@gmail.com>
2023-11-30 08:24:06 +0300

    add process_time_logger

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
@@ -13,6 +15,7 @@ from app.config import settings
 from app.database import engine
 from app.hotels.rooms.router import router as router_rooms
 from app.images.router import router as router_images
+from app.logger import logger
 from app.pages.router import router as router_pages
 from app.users.router import router as router_users
 
@@ -57,3 +60,14 @@ admin.add_view(UsersAdmin)
 admin.add_view(BookingsAdmin)
 admin.add_view(HotelsAdmin)
 admin.add_view(RoomsAdmin)
+
+
+@app.middleware("http")
+async def add_process_time_logger(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logger.info('Request execution time', extra={
+        'process_time': round(process_time, 3)
+    })
+    return response
